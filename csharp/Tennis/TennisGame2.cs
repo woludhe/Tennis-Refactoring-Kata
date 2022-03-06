7f5196533959f4e55a49634a08bf767bf07b372a
Wendy Oludhe <woludhe@rsce.dpko.un.org>
2022-03-06 11:35:28 +0300

    Update TennisGame2.cs
    - Instatiate p1point to 0
    - Use Dictionary instead of if statements to get scores, to improve performance
    - Remove SetP1Score as method and set as parameter adding value to p1point/p2point
    - WonPoint put else if statement for player2 incase someone puts an invalid value
    - Localized global parameter p1res/p2res to GetScore method

--- csharp/Tennis/TennisGame2.cs
+++ csharp/Tennis/TennisGame2.cs
@@ -1,85 +1,82 @@
+using System;
+using System.Collections.Generic;
+
 namespace Tennis
 {
     public class TennisGame2 : ITennisGame
     {
-        private int p1point;
+        private int p1point = 0;
         private int p2point;
+        public int SetP1Score
+        {
+            set { p1point += value; }  
+        }
 
-        private string p1res = "";
-        private string p2res = "";
+        public int SetP2Score
+        {
+            set { p2point += value; }
+        }
+        
         private string player1Name;
         private string player2Name;
 
         public TennisGame2(string player1Name, string player2Name)
         {
             this.player1Name = player1Name;
-            p1point = 0;
             this.player2Name = player2Name;
         }
 
         public string GetScore()
         {
             var score = "";
+            string p1res = "";
+            string p2res = "";
+
+            Dictionary<int, string> scorePoints = new Dictionary<int, string>();
+            scorePoints.Add(0, "Love");
+            scorePoints.Add(1, "Fifteen");
+            scorePoints.Add(2, "Thirty");
+            scorePoints.Add(3, "Forty");
+            scorePoints.Add(4, "Game");
+
             if (p1point == p2point && p1point < 3)
             {
-                if (p1point == 0)
-                    score = "Love";
-                if (p1point == 1)
-                    score = "Fifteen";
-                if (p1point == 2)
-                    score = "Thirty";
+                scorePoints.TryGetValue(p1point, out score);
                 score += "-All";
             }
+
             if (p1point == p2point && p1point > 2)
                 score = "Deuce";
 
             if (p1point > 0 && p2point == 0)
             {
-                if (p1point == 1)
-                    p1res = "Fifteen";
-                if (p1point == 2)
-                    p1res = "Thirty";
-                if (p1point == 3)
-                    p1res = "Forty";
-
+                scorePoints.TryGetValue(p1point, out p1res);
                 p2res = "Love";
                 score = p1res + "-" + p2res;
             }
             if (p2point > 0 && p1point == 0)
             {
-                if (p2point == 1)
-                    p2res = "Fifteen";
-                if (p2point == 2)
-                    p2res = "Thirty";
-                if (p2point == 3)
-                    p2res = "Forty";
-
+                scorePoints.TryGetValue(p2point, out p2res);
                 p1res = "Love";
                 score = p1res + "-" + p2res;
             }
 
             if (p1point > p2point && p1point < 4)
             {
-                if (p1point == 2)
-                    p1res = "Thirty";
-                if (p1point == 3)
-                    p1res = "Forty";
-                if (p2point == 1)
-                    p2res = "Fifteen";
-                if (p2point == 2)
-                    p2res = "Thirty";
+                if (p1point == 2 || p1point == 3)
+                    scorePoints.TryGetValue(p1point, out p1res);
+
+                if (p2point == 1 || p2point == 2)
+                    scorePoints.TryGetValue(p2point, out p2res);
                 score = p1res + "-" + p2res;
             }
             if (p2point > p1point && p2point < 4)
             {
-                if (p2point == 2)
-                    p2res = "Thirty";
-                if (p2point == 3)
-                    p2res = "Forty";
-                if (p1point == 1)
-                    p1res = "Fifteen";
-                if (p1point == 2)
-                    p1res = "Thirty";
+                if (p2point == 2 || p2point == 3)
+                    scorePoints.TryGetValue(p2point, out p2res);
+
+                if (p1point == 1 || p1point == 2)
+                    scorePoints.TryGetValue(p1point, out p1res);
                 score = p1res + "-" + p2res;
             }
 
@@ -104,40 +101,13 @@ namespace Tennis
             return score;
         }
 
-        public void SetP1Score(int number)
-        {
-            for (int i = 0; i < number; i++)
-            {
-                P1Score();
-            }
-        }
-
-        public void SetP2Score(int number)
-        {
-            for (var i = 0; i < number; i++)
-            {
-                P2Score();
-            }
-        }
-
-        private void P1Score()
-        {
-            p1point++;
-        }
-
-        private void P2Score()
-        {
-            p2point++;
-        }
-
         public void WonPoint(string player)
         {
             if (player == "player1")
-                P1Score();
-            else
-                P2Score();
+                SetP1Score = 1;
+            else if (player == "player2")
+                SetP2Score = 1;
         }
-
     }
 }
 
